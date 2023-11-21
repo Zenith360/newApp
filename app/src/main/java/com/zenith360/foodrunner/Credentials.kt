3342c067dcc9ef4535d4d2fe104ca59413c5e37d
Ali Asger <aliasgersaifee2003@gmail.com>
2023-11-21 17:31:01 +0530

    Wrote Kotlin for RegisterActivity and just created an empty ForgotPwd Activity

--- app/src/main/java/com/zenith360/foodrunner/Credentials.kt
+++ app/src/main/java/com/zenith360/foodrunner/Credentials.kt
@@ -1,6 +1,9 @@
 package com.zenith360.foodrunner
 
 object Credentials{
-    var pwdSet = HashSet<String>()
-    var mobNumberSet = HashSet<String>()
+    var pwdSet = hashSetOf<String>("Ali")
+    var mobNumberSet = hashSetOf<String>("8530239527")
+    var nameSet = hashSetOf<String>("Ali Asger Saifee")
+    var emailSet = hashSetOf<String>("ali@ali.com")
+    var addrSet = hashSetOf<String>("Pune")
 }
--- app/src/main/java/com/zenith360/foodrunner/RegisterActivity.kt
+++ app/src/main/java/com/zenith360/foodrunner/RegisterActivity.kt
@@ -1,11 +1,106 @@
 package com.zenith360.foodrunner
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var txtRegYou: TextView
+    private lateinit var etName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etMobileNum: EditText
+    private lateinit var etAddr: EditText
+    private lateinit var etPwd1: EditText
+    private lateinit var etPwd2: EditText
+    private lateinit var btnReg: Button
+
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences =
+            getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+
         setContentView(R.layout.activity_register)
+
+        txtRegYou = findViewById(R.id.txtRegYou)
+        etName = findViewById(R.id.etName)
+        etEmail = findViewById(R.id.etEmail)
+        etMobileNum = findViewById(R.id.etMobileNum)
+        etAddr = findViewById(R.id.etAddr)
+        etPwd1 = findViewById(R.id.etPwd1)
+        etPwd2 = findViewById(R.id.etPwd2)
+        btnReg = findViewById(R.id.btnReg)
+
+        btnReg.setOnClickListener {
+
+            val name = etName.text.toString()
+            val email = etEmail.text.toString()
+            val mobNum = etMobileNum.text.toString()
+            val addr = etAddr.text.toString()
+            val pwd1 = etPwd1.text.toString()
+            val pwd2 = etPwd2.text.toString()
+
+            if (name.length < 3) {
+
+                Toast.makeText(
+                    this@RegisterActivity,
+                    "Your Name is shorter than our min limit!",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                etName.text.clear()
+            }
+
+            if (pwd1 == pwd2) {
+
+                if (pwd1.length < 4) {
+
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Your Password is shorter than our min limit!",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    etPwd1.text.clear()
+                    etPwd2.text.clear()
+                } else {
+
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "You Were Registered Successfully!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    Credentials.nameSet.add(name)
+                    Credentials.emailSet.add(email)
+                    Credentials.mobNumberSet.add(mobNum)
+                    Credentials.addrSet.add(addr)
+                    Credentials.pwdSet.add(pwd1)
+
+                    sharedPreferences.edit().putBoolean("Logged_In", true).apply()
+
+                    val intent = Intent(this@RegisterActivity, )
+                    startActivity(intent)
+                }
+            } else {
+
+                Toast.makeText(
+                    this@RegisterActivity,
+                    "Please Enter Password Again",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                etPwd1.text.clear()
+                etPwd2.text.clear()
+            }
+        }
     }
 }
