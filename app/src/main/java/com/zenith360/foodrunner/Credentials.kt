af66dbd95c4b73b3cca1dfb3395ebbe5f4628502
Ali Asger <aliasgersaifee2003@gmail.com>
2023-11-21 18:02:22 +0530

    Changed var to val in Credentials so that another copy is not created (Hopefully)

--- app/src/main/java/com/zenith360/foodrunner/Credentials.kt
+++ app/src/main/java/com/zenith360/foodrunner/Credentials.kt
@@ -1,9 +1,9 @@
 package com.zenith360.foodrunner
 
 object Credentials{
-    var mobNumberSet = hashSetOf<String>("8530239527")
-    var pwdMap = mutableMapOf<String, String>("8530239527" to "Ali")
-    var nameMap = mutableMapOf<String, String>("8530239527" to "Ali Asger Saifee")
-    var emailMap = mutableMapOf<String, String>("8530239527" to "ali@ali.com")
-    var addrMap = mutableMapOf<String, String>("8530239527" to "Pune")
+    val mobNumberSet = hashSetOf<String>("8530239527")
+    val pwdMap = mutableMapOf<String, String>("8530239527" to "Ali")
+    val nameMap = mutableMapOf<String, String>("8530239527" to "Ali Asger Saifee")
+    val emailMap = mutableMapOf<String, String>("8530239527" to "ali@ali.com")
+    val addrMap = mutableMapOf<String, String>("8530239527" to "Pune")
 }
