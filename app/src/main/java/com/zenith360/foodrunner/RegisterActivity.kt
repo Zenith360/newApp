40ce0f219f02c6a2fb79e196708f8192d0837e8b
Ali Asger <aliasgersaifee2003@gmail.com>
2023-11-21 17:02:53 +0530

    Created Register Layout

---
+++ app/src/main/java/com/zenith360/foodrunner/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.zenith360.foodrunner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
